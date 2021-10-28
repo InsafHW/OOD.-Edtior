5cf8455b8a5516ef8b7491bf74b1c104b710170e
Insaf4ik <insaf.halirahmanoff@yandex.ru>
2021-10-28 23:15:18 +0300

    Перевел данные на std::list. Исправил логику группировки

--- Editor/CompoundShape.h
+++ Editor/CompoundShape.h
@@ -17,6 +17,19 @@ public:
 		m_shapes.remove(child);
 	}
 
+	void RemoveAll()
+	{
+		for (auto it = m_shapes.begin(); it != m_shapes.end(); it++)
+		{
+			m_shapes.remove(*it);
+		}
+	};
+
+	std::list<IShape*> GetShapes()
+	{
+		return m_shapes;
+	};
+
 	void Move(float x, float y)
 	{
 		std::cout << "x = " << x << "; y = " << y << std::endl;
@@ -34,6 +47,83 @@ public:
 			(*it)->Draw(window);
 		}
 	};
+
+	bool IsMouseInGroup(float x, float y)
+	{
+		for (auto it = m_shapes.begin(); it != m_shapes.end(); it++)
+		{
+			if ((*it)->GetGlobalBounds().contains(x, y)) {
+				return true;
+			};
+		}
+		return false;
+	};
+
+	sf::FloatRect GetGlobalBounds()
+	{
+		// find max left x and max right y
+		int minX = 100000;
+		int maxX = 0;
+		int maxY = 0;
+		int minY = 100000;
+
+		for (auto it = m_shapes.begin(); it != m_shapes.end(); it++)
+		{
+			if ((*it)->GetGlobalBounds().left + (*it)->GetGlobalBounds().width > maxX)
+			{
+				maxX = (*it)->GetGlobalBounds().left + (*it)->GetGlobalBounds().width;
+			}
+			if ((*it)->GetGlobalBounds().left < minX)
+			{
+				minX = (*it)->GetGlobalBounds().left;
+			}
+			if ((*it)->GetGlobalBounds().top + (*it)->GetGlobalBounds().height > maxY)
+			{
+				maxY = (*it)->GetGlobalBounds().top + (*it)->GetGlobalBounds().height;
+			}
+			if ((*it)->GetGlobalBounds().top < minY)
+			{
+				minY = (*it)->GetGlobalBounds().top;
+			}
+		}
+
+		sf::RectangleShape rect(sf::Vector2f(maxX - minX, maxY - minY));
+		rect.setPosition(minX, minY);
+		return rect.getGlobalBounds();
+	};
+
+	sf::Vector2f GetOrigin()
+	{
+		int minX = 100000;
+		int maxX = 0;
+		int maxY = 0;
+		int minY = 100000;
+
+		for (auto it = m_shapes.begin(); it != m_shapes.end(); it++)
+		{
+			if ((*it)->GetGlobalBounds().left + (*it)->GetGlobalBounds().width > maxX)
+			{
+				maxX = (*it)->GetGlobalBounds().left + (*it)->GetGlobalBounds().width;
+			}
+			if ((*it)->GetGlobalBounds().left < minX)
+			{
+				minX = (*it)->GetGlobalBounds().left;
+			}
+			if ((*it)->GetGlobalBounds().top + (*it)->GetGlobalBounds().height > maxY)
+			{
+				maxY = (*it)->GetGlobalBounds().top + (*it)->GetGlobalBounds().height;
+			}
+			if ((*it)->GetGlobalBounds().top < minY)
+			{
+				minY = (*it)->GetGlobalBounds().top;
+			}
+		}
+
+		sf::RectangleShape rect(sf::Vector2f(maxX - minX, maxY - minY));
+		rect.setPosition(minX, minY);
+		return rect.getOrigin();
+	};
+
 private:
 	std::list<IShape*> m_shapes;
 };
--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -6,6 +6,9 @@
 #include "OutlineDecorator.h"
 #include "CompoundShape.h"
 #include <iostream>
+#include "Singleton.h"
+
+//Singleton* editor = Singleton::getInstance();
 
 //int main()
 //{
@@ -123,31 +126,251 @@
 
 
 
+//int main()
+//{
+//	//Creating the window
+//	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(1280, 800), "SFML Game");
+//
+//	std::list<IShape*> my_shapes;
+//	std::list<CompoundShape*> compound_shapes;
+//
+//	OutlineDecorator* my_circle1 = new OutlineDecorator(new Circle(40, 40, 100));
+//	OutlineDecorator* my_circle2 = new OutlineDecorator(new Circle(80, 110, 100));
+//	OutlineDecorator* my_rectangle1 = new OutlineDecorator(new Rectangle(10, 34, 190, 344));
+//	OutlineDecorator* my_rectangle2 = new OutlineDecorator(new Rectangle(90, 43, 132, 844));
+//
+//	my_shapes.push_back(my_circle1);
+//	my_shapes.push_back(my_circle2);
+//	my_shapes.push_back(my_rectangle1);
+//	my_shapes.push_back(my_rectangle2);
+//
+//	window->setKeyRepeatEnabled(false);
+//
+//	sf::Event event;
+//
+//	//States for button/events
+//	bool mouseClicked = false;
+//	bool dragging = false;
+//	bool lShiftPressed = false;
+//	bool ctrlPressed = false;
+//	bool hasCompound = false;
+//
+//	sf::Vector2f mouseShapeOffset;
+//
+//	//Variables
+//	int mouseX = 0;
+//	int mouseY = 0;
+//
+//	int dragginItemIdx = -1;
+//	int dragginCompoundIdx = -1;
+//	int selectedItemsCount = 0;
+//
+//	/*for (int i = 0; i < my_shapes.size(); i++)
+//	{
+//		compound.Add(my_shapes[i]);
+//	};*/
+//
+//	//Game loop
+//	while (window->isOpen())
+//	{
+//		//EVENTS
+//		while (window->pollEvent(event))
+//		{
+//			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::LShift)
+//			{
+//				lShiftPressed = true;
+//				std::cout << "Left Shift pressed" << std::endl;
+//			}
+//
+//			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::LShift)
+//			{
+//				lShiftPressed = false;
+//				std::cout << "Left Shift released" << std::endl;
+//			}
+//
+//			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::LControl)
+//			{
+//				ctrlPressed = true;
+//				std::cout << "Left control pressed" << std::endl;
+//			}
+//
+//			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::LControl)
+//			{
+//				ctrlPressed = false;
+//				std::cout << "Left control released" << std::endl;
+//			}
+//
+//			//LMB Pressed
+//			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+//			{
+//				mouseClicked = true;
+//
+//				int id = 0;
+//				int compId = 0;
+//				for (auto it = compound_shapes.begin(); it != compound_shapes.end(); it++)
+//				{
+//					compId++;
+//					if ((*it)->IsMouseInGroup(event.mouseButton.x, event.mouseButton.y))
+//					{
+//						dragginCompoundIdx = compId;
+//					}
+//				}
+//				for (auto it = my_shapes.begin(); it != my_shapes.end(); it++)
+//				{
+//					if ((*it)->GetGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+//					{
+//						std::cout << "Inside shape" << std::endl;
+//						if (selectedItemsCount == 0)
+//						{
+//							(*it)->Select();
+//						}
+//						else if (lShiftPressed)
+//						{
+//							if ((*it)->IsSelected())
+//							{
+//								(*it)->UnSelect();
+//							}
+//							else
+//							{
+//								(*it)->Select();
+//							}
+//						}
+//						dragging = true;
+//						dragginItemIdx = id++;
+//						std::cout << "dragginItemIdx = " << id << std::endl;
+//						mouseShapeOffset.x = event.mouseButton.x - (*it)->GetGlobalBounds().left - (*it)->GetOrigin().x;
+//						mouseShapeOffset.y = event.mouseButton.y - (*it)->GetGlobalBounds().top - (*it)->GetOrigin().y;
+//					}
+//
+//				};
+//			}
+//
+//			/*if (ctrlPressed && event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::U)
+//			{
+//				for (auto it = compound_shapes.begin(); it != compound_shapes.end(); it++)
+//				{
+//					auto shapes = (*it)->GetShapes();
+//					for (auto itInner = shapes.begin(); itInner != shapes.end(); itInner++)
+//					{
+//						shapes.push_back(*itInner);
+//					}
+//					(*it)->RemoveAll();
+//				}
+//			}*/
+//
+//			/*if (ctrlPressed && event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::G)
+//			{
+//				std::cout << "GROUPPING..." << std::endl;
+//				CompoundShape* compound = new CompoundShape();
+//				for (auto it = my_shapes.begin(); it != my_shapes.end(); it++)
+//				{
+//					if ((*it)->IsSelected())
+//					{
+//						compound->Add((*it));
+//						my_shapes.remove(*it);
+//					}
+//				}
+//			}*/
+//
+//			if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left)
+//			{
+//				mouseClicked = false;
+//				dragging = false;
+//			}
+//
+//			//Mouse Moved in window
+//			if (event.type == sf::Event::MouseMoved)
+//			{
+//				mouseX = event.mouseMove.x;
+//				mouseY = event.mouseMove.y;
+//			}
+//
+//			//Event type is window closed
+//			if (event.type == sf::Event::Closed)
+//			{
+//				window->close();
+//			}
+//		}
+//
+//		//LOGIC
+//
+//		if (dragging == true)
+//		{
+//			if (dragginItemIdx != -1)
+//			{
+//				(*std::next(my_shapes.begin(), dragginItemIdx))->SetPosition(mouseX - mouseShapeOffset.x, mouseY - mouseShapeOffset.y);
+//
+//			}
+//		}
+//
+//		//RENDERING
+//		window->clear();
+//
+//		for (auto it = my_shapes.begin(); it != my_shapes.end(); it++)
+//		{
+//			(*it)->Draw(window);
+//		}
+//
+//		window->display();
+//	}
+//
+//	//Close the program
+//	return 0;
+//}
+
+//int main()
+//{
+//	std::vector<IShape*> my_shapes;
+//
+//
+//	OutlineDecorator* my_circle1 = new OutlineDecorator(new Circle(40, 40, 100));
+//	OutlineDecorator* my_circle2 = new OutlineDecorator(new Circle(80, 110, 100));
+//	OutlineDecorator* my_rectangle1 = new OutlineDecorator(new Rectangle(10, 34, 190, 344));
+//	OutlineDecorator* my_rectangle2 = new OutlineDecorator(new Rectangle(90, 43, 132, 844));
+//
+//	my_shapes.push_back(my_circle1);
+//	my_shapes.push_back(my_circle2);
+//	my_shapes.push_back(my_rectangle1);
+//	my_shapes.push_back(my_rectangle2);
+//
+//	auto& game = Singleton::getInstance();
+//	game.AddShape(my_circle1);
+//	game.AddShape(my_circle2);
+//	game.AddShape(my_rectangle1);
+//	game.AddShape(my_rectangle2);
+//	game.Start();
+//};
+
+
+
+
 int main()
 {
 	//Creating the window
 	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(1280, 800), "SFML Game");
-
-	std::vector<IShape*> my_shapes;
-	std::vector<IShape*> selected_shapes;
+	std::list<IShape*> list;
+	std::list<CompoundShape*> compound;
+	std::list<IShape*> selected;
 
 	OutlineDecorator* my_circle1 = new OutlineDecorator(new Circle(40, 40, 100));
 	OutlineDecorator* my_circle2 = new OutlineDecorator(new Circle(80, 110, 100));
 	OutlineDecorator* my_rectangle1 = new OutlineDecorator(new Rectangle(10, 34, 190, 344));
 	OutlineDecorator* my_rectangle2 = new OutlineDecorator(new Rectangle(90, 43, 132, 844));
 
-	my_shapes.push_back(my_circle1);
-	my_shapes.push_back(my_circle2);
-	my_shapes.push_back(my_rectangle1);
-	my_shapes.push_back(my_rectangle2);
+	list.push_back(my_circle1);
+	list.push_back(my_circle2);
+	list.push_back(my_rectangle1);
+	list.push_back(my_rectangle2);
 
 	window->setKeyRepeatEnabled(false);
+
 
 	sf::Event event;
 
 	//States for button/events
 	bool mouseClicked = false;
 	bool dragging = false;
+	bool compoundDragging = false;
 	bool lShiftPressed = false;
 	bool ctrlPressed = false;
 	bool hasCompound = false;
@@ -159,12 +382,8 @@ int main()
 	int mouseY = 0;
 
 	int dragginItemIdx = 0;
-
-	CompoundShape compound;
-	for (int i = 0; i < my_shapes.size(); i++)
-	{
-		compound.Add(my_shapes[i]);
-	};
+	int selectedItemsCount = 0;
+	int compoundDraggingIdx = 0;
 
 	//Game loop
 	while (window->isOpen())
@@ -200,60 +419,93 @@ int main()
 			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
 			{
 				mouseClicked = true;
-
-				for (int i = 0; i < my_shapes.size(); i++)
+				int id = 0;
+				for (auto it = list.begin(); it != list.end(); it++)
 				{
-					if (my_shapes[i]->GetGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+					if ((*it)->GetGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
 					{
-						if (lShiftPressed)
+						if (selectedItemsCount == 0 && compound.size() == 0)
 						{
-							if (my_shapes[i]->IsSelected())
+							if ((*it)->IsSelected())
 							{
-								my_shapes[i]->UnSelect();
+								selectedItemsCount--;
 							}
 							else
 							{
-								my_shapes[i]->Select();
+								(*it)->Select();
+								selectedItemsCount++;
+								selected.push_back(*it);
 							}
 						}
+						else if (lShiftPressed)
+						{
+							if ((*it)->IsSelected())
+							{
+								(*it)->UnSelect();
+								selectedItemsCount--;
+								selected.remove(*it);
+							}
+							else
+							{
+								(*it)->Select();
+								selectedItemsCount++;
+								selected.push_back(*it);
+							}
+						}
+						dragginItemIdx = id;
 						dragging = true;
-						dragginItemIdx = i;
-						mouseShapeOffset.x = event.mouseButton.x - my_shapes[i]->GetGlobalBounds().left - my_shapes[i]->GetOrigin().x;
-						mouseShapeOffset.y = event.mouseButton.y - my_shapes[i]->GetGlobalBounds().top - my_shapes[i]->GetOrigin().y;
+						mouseShapeOffset.x = event.mouseButton.x - (*it)->GetGlobalBounds().left - (*it)->GetOrigin().x;
+						mouseShapeOffset.y = event.mouseButton.y - (*it)->GetGlobalBounds().top - (*it)->GetOrigin().y;
 					}
-
-				};
-			}
-
-			if (ctrlPressed && event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::U)
-			{
-				for (int i = 0; i < my_shapes.size(); i++)
-				{
-					if (my_shapes[i]->IsSelected())
-					{
-						compound.Remove(my_shapes[i]);
-					}
+					id++;
 				}
-				hasCompound = false;
+				id = 0;
+				compoundDragging = false;
+				for (auto it = compound.begin(); it != compound.end(); it++)
+				{
+					if ((*it)->IsMouseInGroup(event.mouseButton.x, event.mouseButton.y))
+					{
+						compoundDraggingIdx = id;
+						std::cout << "HERRRRR";
+						compoundDragging = true;
+						mouseShapeOffset.x = event.mouseButton.x - (*it)->GetGlobalBounds().left - (*it)->GetOrigin().x;
+						mouseShapeOffset.y = event.mouseButton.y - (*it)->GetGlobalBounds().top - (*it)->GetOrigin().y;
+						break;
+					}
+					id++;
+				}
 			}
 
 			if (ctrlPressed && event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::G)
 			{
 				std::cout << "GROUPPING..." << std::endl;
-				for (int i = 0; i < my_shapes.size(); i++)
+				CompoundShape* comp = new CompoundShape();
+				for (auto it = selected.begin(); it != selected.end(); it++)
 				{
-					if (my_shapes[i]->IsSelected())
-					{
-						compound.Add(my_shapes[i]);
-					}
+					list.remove(*it);
+					comp->Add(*it);
 				}
-				hasCompound = true;
+				compound.push_back(comp);
+				selected.clear();
+			}
+
+			if (ctrlPressed && event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::U)
+			{
+				std::cout << "UNGROUPPING..." << std::endl;
+				auto shapes = (*std::next(compound.begin(), compoundDraggingIdx))->GetShapes();
+				for (auto it = shapes.begin(); it != shapes.end(); it++)
+				{
+					(*it)->UnSelect();
+					list.push_back(*it);
+				}
+				compound.remove(*std::next(compound.begin(), compoundDraggingIdx));
 			}
 
 			if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left)
 			{
 				mouseClicked = false;
 				dragging = false;
+				compoundDragging = false;
 			}
 
 			//Mouse Moved in window
@@ -270,27 +522,32 @@ int main()
 			}
 		}
 
-		//LOGIC
 
-		if (dragging == true && my_shapes[dragginItemIdx]->IsSelected())
+		// LOGIC
+		if (dragging == true)
 		{
-			if (hasCompound)
+			std::cout << "x = " << mouseX << " ; y = " << mouseY << std::endl;
+			if ((*std::next(list.begin(), dragginItemIdx))->IsSelected())
 			{
-				compound.Move(mouseX - mouseShapeOffset.x, mouseY - mouseShapeOffset.y);
+				(*std::next(list.begin(), dragginItemIdx))->SetPosition(mouseX - mouseShapeOffset.x, mouseY - mouseShapeOffset.y);
 			}
-			else
-			{
-				my_shapes[dragginItemIdx]->SetPosition(mouseX - mouseShapeOffset.x, mouseY - mouseShapeOffset.y);
-
-			}
+		}
+		if (compoundDragging == true)
+		{
+			(*std::next(compound.begin(), compoundDraggingIdx))->Move(mouseX - mouseShapeOffset.x, mouseY - mouseShapeOffset.y);
+			std::cout << "ZDEC";
 		}
 
 		//RENDERING
 		window->clear();
 
-		for (int i = 0; i < my_shapes.size(); i++)
+		for (auto it = list.begin(); it != list.end(); it++)
 		{
-			my_shapes[i]->Draw(window);
+			(*it)->Draw(window);
+		}
+		for (auto it = compound.begin(); it != compound.end(); it++)
+		{
+			(*it)->Draw(window);
 		}
 
 		window->display();
